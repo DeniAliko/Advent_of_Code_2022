02696fcf1ebfa9da76ea4f8e0e3b5be458326e03
DeniAliko <denialiko2016@gmail.com>
2022-12-07 07:50:42 -0500

    Recursion is broken

--- AOC7.py
+++ AOC7.py
@@ -1,5 +1,39 @@
+import sys
+sys.setrecursionlimit(20954)
+
 file = open("AOC7.txt")
 inputFile = []
 linesInFile = file.readlines()
 for i in linesInFile:
     inputFile.append(format(i.strip()))
+
+def getSize(directory, file):
+    size = 0
+    for i in range(file.index(directory) + 2, len(file)):
+        if file[i][0] == "d":
+            size += getSize("$ cd " + file[i].replace("dir ", ""), file)
+        elif file[i][0] == "$":
+            break
+        else:
+            size += int(file[i].split(" ")[0])
+    return size
+
+file = open("AOC7.txt")
+testCase = []
+linesInTest = file.readlines()
+for i in linesInTest:
+    testCase.append(format(i.strip()))
+
+sizes = []
+for line in testCase:
+    if line[0:4] == "$ cd":
+        # sizes.append(getSize(line, inputFile))
+        sizes.append(getSize(line, testCase))
+        print("The Function was called!")
+
+part1Answer = 0
+for i in sizes:
+    if i <= 100000:
+        part1Answer += i
+
+print(part1Answer)
